0298e45e98a0f4e2844da94f474e8c3b48286e12
itechtamizhan <34471962+itechtamizhan@users.noreply.github.com>
2017-12-24 13:54:39 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,79 +2,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var db = require("./mongo/mongooperation.js");
 var app = express();
 var port = (process.env.PORT || process.env.VCAP_APP_PORT || 8989);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.post('/api/create',function(req,res){
+app.get('/api/create',function(req,res){
 	
-	var request = req.body ;
-	console.log("***"+JSON.stringify(request));
-	if(request.body){
-		request.body = JSON.parse(request.body);
-	}
-	db.insertcollection(request,function(err,result){
-		var resp = {};
-		if(err){
-			resp.code = 400 ;
-			resp.message = "exception occurs.";
-			resp.error = err;
-			return res.send(resp);
-		}
-		resp.code = 200 ;
-		resp.message = "success";
-		resp.result= result ;
-	return res.send(result);
-
-	});
+	return res.send({"Test":true});
 	
 
 });
 
 
-
-app.get('/api/retrieveall',function(req,res){
-	
-	db.retrieveCollection({},function(err,result){
-	var resp = {};
-
-		if(err){
-			resp.code = 400 ;
-			resp.message = "exception occurs.";
-			resp.error = err ;
-			return res.send(resp);
-		}
-		
-		resp.code = 200 ;
-		resp.message = "success";
-		resp.result= result ;
-	return res.send(result);
-	});
-});
-
-app.get('/api/:id',function(req,res){
-	var id = req.params.id ;
-	var query = {};
-	query._id = id ;
-	db.retrieveOneCollection(query,function(err,result){
-	var resp = {};
-
-		if(err){
-			resp.code = 400 ;
-			resp.message = "exception occurs.";
-			resp.error = err ;
-			return res.send(resp);
-		}	
-		
-		resp.code = 200 ;
-		resp.message = "success";
-		resp.result= result ;
-	return res.send(result);
-	});
-});
 
 
 var server = app.listen(port,function(){
